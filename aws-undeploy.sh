bab9658840fa4a645ee9c0ee42b7754f4d666494
luzi82 <luzi82@gmail.com>
2020-08-11 17:12:53 +0800

    aws deploy script

---
+++ aws-undeploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+MY_PATH=${PWD}
+
+cd ${MY_PATH}
+. _env.sh
+
+cd ${MY_PATH}
+rm -rf venv-aws-deploy
+rm -rf package-lock.json
+rm -rf node_modules
+
+cd ${MY_PATH}
+npm install serverless serverless-python-requirements
+npm update serverless serverless-python-requirements
+SERVERLESS=${MY_PATH}/node_modules/.bin/serverless
+${SERVERLESS} --version
+
+cd ${MY_PATH}
+python3 -m venv venv-aws-deploy
+. venv-aws-deploy/bin/activate
+pip install --upgrade pip wheel
+pip install awscli
+
+cd ${MY_PATH}/src
+${SERVERLESS} remove
+
+cd ${MY_PATH}
+deactivate
+rm -rf venv-aws-deploy
+rm -rf package-lock.json
+rm -rf node_modules
--- local-test.sh
+++ local-test.sh
@@ -1,11 +1,17 @@
+#!/bin/bash
+
 PROJECT_ROOT_PATH=${PWD}
+
+# clean up
+cd ${PROJECT_ROOT_PATH}
+rm -rf venv-local-test
 
 # init local run env
 cd ${PROJECT_ROOT_PATH}
-python3 -m venv venv
-. venv/bin/activate
+python3 -m venv venv-local-test
+. venv-local-test/bin/activate
 pip install --upgrade pip wheel
-pip install -r requirement.txt
+pip install -r src/requirement.txt
 
 # local run
 cd ${PROJECT_ROOT_PATH}/src
