cc46babeb5c9a208e3b3fbeeed4f19e5387a24d3
luzi82 <luzi82@gmail.com>
2020-10-01 15:49:22 +0800

    endpoint read private static s3

--- src/endpoint.py
+++ src/endpoint.py
@@ -1,12 +1,20 @@
 import flask
+import futsu.json
+import futsu.storage
 import os
 
 app = flask.Flask(__name__)
 
 STAGE = os.environ['STAGE']
+CONF_PATH = os.environ['CONF_PATH']
 
 @app.route('/')
 def index():
     return flask.render_template('index.tmpl',
         STAGE=STAGE,
     )
+
+@app.route('/deploy_token')
+def get_deploy_token():
+    conf_data = futsu.json.path_to_data(futsu.storage.join(CONF_PATH,'conf.json'))
+    return conf_data['DEPLOY_TOKEN']
