37e7dd059fc40d828d5b5f3e11264eaefc2f57c0
luzi82 <luzi82@gmail.com>
2020-10-01 23:50:54 +0800

    write to bucket, enable listbucket

--- src/endpoint.py
+++ src/endpoint.py
@@ -1,3 +1,4 @@
+import datetime
 import flask
 import futsu.json
 import futsu.storage
@@ -7,11 +8,24 @@ app = flask.Flask(__name__)
 
 STAGE = os.environ['STAGE']
 CONF_PATH = os.environ['CONF_PATH']
+PUBLIC_STATIC_PATH   = os.environ['PUBLIC_STATIC_PATH']
+PUBLIC_MUTABLE_PATH  = os.environ['PUBLIC_MUTABLE_PATH']
+PRIVATE_STATIC_PATH  = os.environ['PRIVATE_STATIC_PATH']
+PRIVATE_MUTABLE_PATH = os.environ['PRIVATE_MUTABLE_PATH']
 
 @app.route('/')
 def index():
+    now_ts = int(datetime.datetime.now().timestamp())
+    timestamp_path = futsu.storage.join(PRIVATE_MUTABLE_PATH,'timestamp')
+    print(timestamp_path)
+
+    last_ts = futsu.storage.path_to_bytes(timestamp_path).decode('utf-8') if futsu.storage.is_blob_exist(timestamp_path) else -1
+    futsu.storage.bytes_to_path(timestamp_path,f'{now_ts}'.encode('utf-8'))
+
     return flask.render_template('index.tmpl',
         STAGE=STAGE,
+        LAST_TS=last_ts,
+        NOW_TS=now_ts,
     )
 
 @app.route('/compute_domain')
