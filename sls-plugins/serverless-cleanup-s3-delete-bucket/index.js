1cd7ea92e254e2d0b2d7175f9277389a408486ff
luzi82 <luzi82@gmail.com>
2020-10-06 00:11:39 +0800

    delete s3 on clean up in dev

---
+++ sls-plugins/serverless-cleanup-s3-delete-bucket/index.js
@@ -0,0 +1,150 @@
+'use strict';
+
+const chalk = require('chalk');
+const messagePrefix = 'S3 Delete Bucket: ';
+
+class ServerlessCleanupS3DeleteBucket {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options = options;
+    this.provider = this.serverless.getProvider('aws');
+
+    this.commands = {
+      s3DeleteBucket: {
+        usage: 'Delete S3 bucket',
+        lifecycleEvents: ['s3_delete_bucket'],
+        options: {
+          verbose: {
+            usage: 'Increase verbosity',
+            shortcut: 'v'
+          }
+        }
+      },
+    };
+
+    this.hooks = {
+      'deploy:cleanup': this.s3DeleteBucket.bind(this),
+      's3DeleteBucket:s3_delete_bucket': this.s3DeleteBucket.bind(this),
+    };
+  }
+
+  log(message) {
+    if (this.options.verbose) {
+      this.serverless.cli.log(message);
+    }
+  }
+
+  s3DeleteBucket() {
+    const self = this;
+
+    const getAwsBucketList = () => {
+      return self.provider.request('S3', 'listBuckets').then((result)=>{
+        return new Promise((resolve) => {
+          resolve(result.Buckets.map((item)=>{
+            return item.Name;
+          }));
+        });
+      });
+    };
+    const getAllKeys = (bucket) => {
+      const get = (src = {}) => {
+        const data = src.data;
+        const keys = src.keys || [];
+        const param = {
+          Bucket: bucket
+        };
+        if (data) {
+          param.ContinuationToken = data.NextContinuationToken;
+        }
+        return self.provider.request('S3', 'listObjectsV2', param).then((result) => {
+          return new Promise((resolve) => {
+            resolve({
+              data: result, keys: keys.concat(result.Contents.map((item) => {
+                return item.Key;
+              }))
+            });
+          });
+        });
+      };
+      const list = (src = {}) => {
+        return get(src).then((result) => {
+          if (result.data.IsTruncated) {
+            return list(result);
+          } else {
+            const keys = result.keys;
+            const batched = [];
+            for (let i = 0; i < keys.length; i += 1000) {
+              const objects = keys.slice(i, i + 1000).map((item) => {
+                return {Key: item};
+              });
+              batched.push({
+                Bucket: bucket,
+                Delete: {
+                  Objects: objects
+                }
+              });
+            }
+            return new Promise((resolve) => {
+              resolve(batched);
+            });
+          }
+        });
+      };
+      return list();
+    };
+    const executeRemove = (params) => {
+      return Promise.all(params.map(param => {
+        return self.provider.request('S3', 'deleteObjects', param);
+      }));
+    };
+    const executeDeleteBucket = (bucket) => {
+      self.log(`executeDeleteBucket ${bucket}`);
+      return self.provider.request('S3', 'deleteBucket', {Bucket: bucket});
+    };
+
+    const populateConfig = () => {
+      return this.serverless.variables.populateObject(this.serverless.service.custom.cleanupS3DeleteBucket)
+        .then(fileConfig => {
+          const defaultConfig = {
+            enable: true,
+            bucketList: [],
+          };
+          return Object.assign({}, defaultConfig, fileConfig);
+        });
+    };
+
+    return new Promise((resolve) => {
+      return populateConfig().then(config => {
+        if (!config.enable) { return Promise.all([]).then(resolve); }
+        const enableBucketList = config.bucketList.filter(i=>((!('enable' in i))||(i['enable'])));
+        if (enableBucketList.length<=0) { return Promise.all([]).then(resolve); }
+        return getAwsBucketList().then(awsBucketList=>{
+          // self.log(awsBucketList);
+          const awsBucketSet = new Set(awsBucketList);
+          const processBucketList = enableBucketList.filter(i=>awsBucketSet.has(i.bucketName));
+          const processBucketNameList = processBucketList.map(i=>i.bucketName);
+          self.log(`processBucketNameList: ${processBucketNameList}`);
+          let promisses = [];
+          for (const bucketData of processBucketList) {
+            const bucketName = bucketData.bucketName;
+            promisses.push(getAllKeys(bucketName).then(executeRemove).then(() => {
+              return executeDeleteBucket(bucketName);
+            }).then(() => {
+              const message = `Success: ${bucketName} is deleted.`;
+              self.log(message);
+              self.serverless.cli.consoleLog(`${messagePrefix}${chalk.yellow(message)}`);
+            }).catch((err) => {
+              const message = `Fail: ${bucketName} may not be deleted.`;
+              self.log(message);
+              self.log(err);
+              self.serverless.cli.consoleLog(`${messagePrefix}${chalk.yellow(message)}`);
+            }));
+          }
+          return Promise.all(promisses).then(resolve);
+        });
+      });
+    });
+  }
+}
+
+module.exports = ServerlessCleanupS3DeleteBucket;
