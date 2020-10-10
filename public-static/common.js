3172eca362167b47812fdf9ce3b859fd062e2a61
luzi82 <luzi82@gmail.com>
2020-10-10 22:12:08 +0800

    refactor

---
+++ public-static/common.js
@@ -0,0 +1,24 @@
+// https://stackoverflow.com/questions/133925/javascript-post-request-like-a-form-submit
+function post(path, params, method='post') {
+
+  // The rest of this code assumes you are not using a library.
+  // It can be made less wordy if you use one.
+  const form = document.createElement('form');
+  form.style.display = 'none';
+  form.method = method;
+  form.action = path;
+
+  for (const key in params) {
+    if (params.hasOwnProperty(key)) {
+      const hiddenField = document.createElement('input');
+      hiddenField.type = 'hidden';
+      hiddenField.name = key;
+      hiddenField.value = params[key];
+
+      form.appendChild(hiddenField);
+    }
+  }
+
+  document.body.appendChild(form);
+  form.submit();
+}
