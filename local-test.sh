9867480cf5bc687cd56506d1797fcae389e5e411
luzi82 <luzi82@gmail.com>
2020-11-20 13:40:50 +0000

    local-test go gitpod

--- local-test.sh
+++ local-test.sh
@@ -2,18 +2,12 @@
 
 PROJECT_ROOT_PATH=${PWD}
 
-# clean up
-cd ${PROJECT_ROOT_PATH}
-rm -rf venv-local-test
-
 # init local run env
 cd ${PROJECT_ROOT_PATH}
-python3 -m venv venv-local-test
-. venv-local-test/bin/activate
-pip install --upgrade pip wheel
-pip install -r src/requirements.txt
+pipenv --three
+pipenv install -r src/requirements.txt
 
 # local run
-cd ${PROJECT_ROOT_PATH}/src
-export FLASK_APP=endpoint.py
-flask run
+export FLASK_APP=src/endpoint.py
+cd ${PROJECT_ROOT_PATH}
+pipenv run flask run
--- tasks/env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export PROJECT_PATH="$( cd "$( dirname "$( dirname "${BASH_SOURCE[0]}" )" )" >/dev/null 2>&1 && pwd )"
-export SERVERLESS=${PROJECT_PATH}/node_modules/.bin/serverless
-export FLASK_APP=src/endpoint.py
