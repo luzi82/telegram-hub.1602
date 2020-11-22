db8692bedb5382cbba34b2598c421d5de91cf0aa
luzi82 <luzi82@gmail.com>
2020-11-22 15:35:50 +0000

    ci for sample

--- ci/deploy-sample.sh
+++ ci/deploy-sample.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-export STAGE="ci"
+export STAGE="sample"
 
-cat ci/public-key.gpg | gpg --import
+TMP_FILE=`mktemp`
+trap "{ rm -f ${TMP_FILE}; }" EXIT
 
-curl https://raw.githubusercontent.com/luzi82/codelog.flask.ci.secret/master/secret.tar.gz.gpg.sig | \
-gpg --decrypt | \
-gpg --quiet --batch --yes --decrypt --passphrase="${CI_SECRET}" | \
+curl https://raw.githubusercontent.com/luzi82/codelog.flask.ci.secret/sample-ci/secret.tar.gz.gpg.sig -o ${TMP_FILE}
+gpg --no-default-keyring --keyring ${PWD}/ci/sample-public-key.gpg --verify ${TMP_FILE}
+gpg --no-default-keyring --keyring ${PWD}/ci/sample-public-key.gpg --decrypt ${TMP_FILE} | \
+gpg --quiet --batch --yes --decrypt --passphrase="${SAMPLE_CI_SECRET}" | \
 tar xzf -
 
 . secret/env.sh
-
-cp ci/conf.json conf/conf.json
 
 ./aws-deploy.sh
