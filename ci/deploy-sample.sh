c8aadd252d4d2bae266f2e7faf79f5c5407cea13
luzi82 <luzi82@gmail.com>
2020-11-22 15:43:20 +0000

    fix sample ci

--- ci/deploy-sample.sh
+++ ci/deploy-sample.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+set -e
 
 export STAGE="sample"
 
 TMP_FILE=`mktemp`
 trap "{ rm -f ${TMP_FILE}; }" EXIT
 
-curl https://raw.githubusercontent.com/luzi82/codelog.flask.ci.secret/sample-ci/secret.tar.gz.gpg.sig -o ${TMP_FILE}
+curl https://raw.githubusercontent.com/luzi82/codelog.web-template.1601.secret/sample-ci/secret.tar.gz.gpg.sig -o ${TMP_FILE}
 gpg --no-default-keyring --keyring ${PWD}/ci/sample-public-key.gpg --verify ${TMP_FILE}
 gpg --no-default-keyring --keyring ${PWD}/ci/sample-public-key.gpg --decrypt ${TMP_FILE} | \
 gpg --quiet --batch --yes --decrypt --passphrase="${SAMPLE_CI_SECRET}" | \
