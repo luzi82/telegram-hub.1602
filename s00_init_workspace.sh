504d83b8fdcf4f2fd4ea10edf2fdff51f3664358
luzi82 <luzi82@gmail.com>
2020-12-19 05:43:40 +0000

    init create env folder

--- s00_init_workspace.sh
+++ s00_init_workspace.sh
@@ -6,6 +6,9 @@
 cd ${PROJECT_ROOT_PATH}
 rm -rf ${PROJECT_ROOT_PATH}/dev_env
 
+# init folder
+mkdir -p ${PROJECT_ROOT_PATH}/dev_env
+
 # init local run env
 cd ${PROJECT_ROOT_PATH}
 python3 -m venv ${PROJECT_ROOT_PATH}/dev_env/venv
