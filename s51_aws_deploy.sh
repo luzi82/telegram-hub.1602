5c7856eb35738606436286d4e2be786e6a02ce30
luzi82 <luzi82@gmail.com>
2020-12-19 07:34:37 +0000

    fix deploy

--- s51_aws_deploy.sh
+++ s51_aws_deploy.sh
@@ -27,6 +27,7 @@ ${SERVERLESS} --version
 cd ${PROJECT_ROOT_PATH}/src
 cp ${PROJECT_ROOT_PATH}/src/requirements.txt ${MY_TMP_DIR_PATH}/
 cp --parents `find -name \*.py` ${MY_TMP_DIR_PATH}/
+cp --parents `find -name \*.tmpl` ${MY_TMP_DIR_PATH}/
 
 cd ${MY_TMP_DIR_PATH}
 cp ${PROJECT_ROOT_PATH}/aws/serverless.yml ${MY_TMP_DIR_PATH}/
