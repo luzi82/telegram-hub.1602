8d3ece35c8a181e123b3737a0c8e5b077cfb0a37
luzi82 <luzi82@gmail.com>
2020-12-19 08:18:15 +0000

    fix filename

--- ci/deploy-sample.sh
+++ ci/deploy-sample.sh
@@ -9,7 +9,7 @@ curl https://raw.githubusercontent.com/luzi82/codelog.web-template.1601.secret/s
 gpg --no-default-keyring --keyring ci/sample-public-key.gpg --verify ${TMP_DIR}/secret.tar.gz.gpg.sig
 
 gpg --no-default-keyring --keyring ci/sample-public-key.gpg --decrypt ${TMP_DIR}/secret.tar.gz.gpg.sig | \
-gpg --quiet --batch --yes --decrypt --passphrase="${CI_SECRET}" | \
+gpg --quiet --batch --yes --decrypt --passphrase="${SAMPLE_CI_SECRET}" | \
 tar xzf -
 
 . secret/env.sh
