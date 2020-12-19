ddd6b84ff86b777d39472c9e39ef68801749300d
luzi82 <luzi82@gmail.com>
2020-12-19 05:23:22 +0000

    dev to dev_env

--- s00_init_workspace.sh
+++ s00_init_workspace.sh
@@ -4,11 +4,11 @@
 
 # clean up
 cd ${PROJECT_ROOT_PATH}
-rm -rf dev
+rm -rf dev_env
 
 # init local run env
 cd ${PROJECT_ROOT_PATH}
-python3 -m venv dev/venv
-. dev/venv/bin/activate
+python3 -m venv dev_env/venv
+. dev_env/venv/bin/activate
 pip install --upgrade pip wheel
 pip install -r src/requirements.txt
--- s01_local_run.sh
+++ s01_local_run.sh
@@ -2,8 +2,8 @@
 
 . _env.sh
 
-. ${PROJECT_ROOT_PATH}/dev/venv/bin/activate
+. ${PROJECT_ROOT_PATH}/dev_env/venv/bin/activate
 
 cd ${PROJECT_ROOT_PATH}/src
 export FLASK_APP=${PROJECT_ROOT_PATH}/src/endpoint.py
-${PROJECT_ROOT_PATH}/dev/venv/bin/flask run
+${PROJECT_ROOT_PATH}/dev_env/venv/bin/flask run
