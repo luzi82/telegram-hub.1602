d1b420700a8dee093f60b452ac7286872aed42c9
luzi82 <luzi82@gmail.com>
2020-12-19 12:25:51 +0000

    improve run local

--- s01_dev_run_local.sh
+++ s01_dev_run_local.sh
@@ -7,6 +7,12 @@ MY_TMP_DIR_PATH=${PROJECT_ROOT_PATH}/dev.local.tmp
 export AWS_ACCESS_KEY_ID=XXXXXXXXXXXXXXXXXXXX
 export AWS_SECRET_ACCESS_KEY=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
+DYNAMODB_PORT=8000
+PUBLIC_STATIC_PORT=8100
+PUBLIC_DEPLOYGEN_PORT=8101
+PUBLIC_MUTABLE_PORT=8102
+PUBLIC_TMP_PORT=8103
+
 # clean up
 cd ${PROJECT_ROOT_PATH}
 kill_pid ${MY_TMP_DIR_PATH}/dynamodb.pid
@@ -19,6 +25,32 @@ mkdir -p ${MY_TMP_DIR_PATH}
 
 # activate venv
 . ${PROJECT_ROOT_PATH}/dev_env/venv/bin/activate
+
+# load env var
+export STAGE=local
+export CONF_PATH=${PROJECT_ROOT_PATH}/stages/local/conf.json
+if [ -z ${GITPOD_REPO_ROOT+x} ]; then
+  export PUBLIC_STATIC_URL_PREFIX=`gp url ${PUBLIC_STATIC_PORT}`
+  export PUBLIC_DEPLOYGEN_URL_PREFIX=`gp url ${PUBLIC_DEPLOYGEN_PORT}`
+  export PUBLIC_MUTABLE_URL_PREFIX=`gp url ${PUBLIC_MUTABLE_PORT}`
+  export PUBLIC_TMP_URL_PREFIX=`gp url ${PUBLIC_TMP_PORT}`
+else
+  export PUBLIC_STATIC_URL_PREFIX="http://localhost:${PUBLIC_STATIC_PORT}"
+  export PUBLIC_DEPLOYGEN_URL_PREFIX="http://localhost:${PUBLIC_DEPLOYGEN_PORT}"
+  export PUBLIC_MUTABLE_URL_PREFIX="http://localhost:${PUBLIC_MUTABLE_PORT}"
+  export PUBLIC_TMP_URL_PREFIX="http://localhost:${PUBLIC_TMP_PORT}"
+fi
+export PUBLIC_STATIC_PATH=${PROJECT_ROOT_PATH}/public-static
+export PUBLIC_DEPLOYGEN_PATH=${MY_TMP_DIR_PATH}/public-deploygen
+export PUBLIC_MUTABLE_PATH=${MY_TMP_DIR_PATH}/public-mutable
+export PUBLIC_TMP_PATH=${MY_TMP_DIR_PATH}/public-tmp
+export PRIVATE_STATIC_PATH=${PROJECT_ROOT_PATH}/private-static
+export PRIVATE_DEPLOYGEN_PATH=${MY_TMP_DIR_PATH}/private-deploygen
+export PRIVATE_MUTABLE_PATH=${MY_TMP_DIR_PATH}/private-mutable
+export PRIVATE_TMP_PATH=${MY_TMP_DIR_PATH}/private-tmp
+export DB_TABLE_NAME=tmp_table
+export DYNAMODB_ENDPOINT_URL="http://localhost:${DYNAMODB_PORT}"
+export DYNAMODB_REGION=`jq -r  .AWS_REGION ${PROJECT_ROOT_PATH}/stages/local/conf.json`
 
 # update dynamodb local
 mkdir -p ${MY_TMP_DIR_PATH}
@@ -35,7 +67,7 @@ echo "${TMP0} ${PROJECT_ROOT_PATH}/dev_env/dynamodb_local_latest.tar.gz" | sha25
 cd ${PROJECT_ROOT_PATH}
 mkdir -p ${MY_TMP_DIR_PATH}/dynamodb_local
 cd ${MY_TMP_DIR_PATH}/dynamodb_local
-tar -xzvf ${PROJECT_ROOT_PATH}/dev_env/dynamodb_local_latest.tar.gz
+tar -xzf ${PROJECT_ROOT_PATH}/dev_env/dynamodb_local_latest.tar.gz
 
 # run dynamodb local
 cd ${MY_TMP_DIR_PATH}
@@ -44,7 +76,6 @@ echo $! > dynamodb.pid
 
 # load dynamodb setting
 cd ${PROJECT_ROOT_PATH}
-jq -r  .AWS_REGION ${PROJECT_ROOT_PATH}/stages/local/conf.json | tr -d '\n' > ${MY_TMP_DIR_PATH}/region
 yq -cM .resources.Resources.Db.Properties.AttributeDefinitions   ${PROJECT_ROOT_PATH}/aws/serverless.yml | tr -d '\n' > ${MY_TMP_DIR_PATH}/db.AttributeDefinitions
 yq -cM .resources.Resources.Db.Properties.KeySchema              ${PROJECT_ROOT_PATH}/aws/serverless.yml | tr -d '\n' > ${MY_TMP_DIR_PATH}/db.KeySchema
 yq -cM .resources.Resources.Db.Properties.GlobalSecondaryIndexes ${PROJECT_ROOT_PATH}/aws/serverless.yml | tr -d '\n' > ${MY_TMP_DIR_PATH}/db.GlobalSecondaryIndexes
@@ -58,44 +89,29 @@ aws dynamodb create-table \
     --key-schema file://${MY_TMP_DIR_PATH}/db.KeySchema \
     --global-secondary-indexes file://${MY_TMP_DIR_PATH}/db.GlobalSecondaryIndexes \
     --billing-mode file://${MY_TMP_DIR_PATH}/db.BillingMode \
-    --endpoint-url http://localhost:8000 \
-    --region `cat ${MY_TMP_DIR_PATH}/region`
+    --endpoint-url "${DYNAMODB_ENDPOINT_URL}" \
+    --region "${DYNAMODB_REGION}"
 aws dynamodb wait table-exists \
     --table-name tmp_table \
-    --endpoint-url http://localhost:8000 \
-    --region `cat ${MY_TMP_DIR_PATH}/region`
+    --endpoint-url "${DYNAMODB_ENDPOINT_URL}" \
+    --region "${DYNAMODB_REGION}"
 
 # emulate bucket
 cd ${PROJECT_ROOT_PATH}
-mkdir ${MY_TMP_DIR_PATH}/public-mutable
-mkdir ${MY_TMP_DIR_PATH}/public-deploygen
-mkdir ${MY_TMP_DIR_PATH}/public-tmp
-mkdir ${MY_TMP_DIR_PATH}/private-mutable
-mkdir ${MY_TMP_DIR_PATH}/private-deploygen
-mkdir ${MY_TMP_DIR_PATH}/private-tmp
-python -m http.server 8100 --directory ${PROJECT_ROOT_PATH}/public-static &
+mkdir -p ${PUBLIC_DEPLOYGEN_PATH}
+mkdir -p ${PUBLIC_MUTABLE_PATH}
+mkdir -p ${PUBLIC_TMP_PATH}
+mkdir -p ${PRIVATE_DEPLOYGEN_PATH}
+mkdir -p ${PRIVATE_MUTABLE_PATH}
+mkdir -p ${PRIVATE_TMP_PATH}
+python -m http.server ${PUBLIC_STATIC_PORT}    --directory ${PUBLIC_STATIC_PATH} &
 echo $! > ${MY_TMP_DIR_PATH}/public-static.pid
-python -m http.server 8101 --directory ${MY_TMP_DIR_PATH}/public-mutable &
-echo $! > ${MY_TMP_DIR_PATH}/public-mutable.pid
-python -m http.server 8102 --directory ${MY_TMP_DIR_PATH}/public-deploygen &
+python -m http.server ${PUBLIC_MUTABLE_PORT}   --directory ${PUBLIC_DEPLOYGEN_PATH} &
 echo $! > ${MY_TMP_DIR_PATH}/public-deploygen.pid
-python -m http.server 8103 --directory ${MY_TMP_DIR_PATH}/public-tmp &
+python -m http.server ${PUBLIC_DEPLOYGEN_PORT} --directory ${PUBLIC_MUTABLE_PATH} &
+echo $! > ${MY_TMP_DIR_PATH}/public-mutable.pid
+python -m http.server ${PUBLIC_TMP_PORT}       --directory ${PUBLIC_TMP_PATH} &
 echo $! > ${MY_TMP_DIR_PATH}/public-tmp.pid
-
-# load env var
-export STAGE=local
-export CONF_PATH=${PROJECT_ROOT_PATH}/stages/local/conf.json
-export PUBLIC_STATIC_PATH=${PROJECT_ROOT_PATH}/public-static
-export PUBLIC_DEPLOYGEN_PATH=${MY_TMP_DIR_PATH}/public-deploygen
-export PUBLIC_MUTABLE_PATH=${MY_TMP_DIR_PATH}/public-mutable
-export PUBLIC_TMP_PATH=${MY_TMP_DIR_PATH}/public-tmp
-export PRIVATE_STATIC_PATH=${PROJECT_ROOT_PATH}/private-static
-export PRIVATE_DEPLOYGEN_PATH=${MY_TMP_DIR_PATH}/private-deploygen
-export PRIVATE_MUTABLE_PATH=${MY_TMP_DIR_PATH}/private-mutable
-export PRIVATE_TMP_PATH=${MY_TMP_DIR_PATH}/private-tmp
-export DB_TABLE_NAME=tmp_table
-export DYNAMODB_ENDPOINT_URL=http://localhost:8000
-export DYNAMODB_REGION=`cat ${MY_TMP_DIR_PATH}/region`
 
 # local run
 cd ${PROJECT_ROOT_PATH}/src
