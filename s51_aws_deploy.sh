2149c88135699a23eb53f78090925c9eeb351d92
luzi82 <luzi82@gmail.com>
2020-12-20 08:44:13 +0000

    aws add deploygen

--- s51_aws_deploy.sh
+++ s51_aws_deploy.sh
@@ -24,11 +24,17 @@ ${SERVERLESS} --version
 
 . ${PROJECT_ROOT_PATH}/aws_env/venv/bin/activate
 
+# clone compute file
 cd ${PROJECT_ROOT_PATH}/src
 cp ${PROJECT_ROOT_PATH}/src/requirements.txt ${MY_TMP_DIR_PATH}/
 cp --parents `find -name \*.py` ${MY_TMP_DIR_PATH}/
 cp --parents `find -name \*.tmpl` ${MY_TMP_DIR_PATH}/
 
+# deploygen
+cd ${PROJECT_ROOT_PATH}
+${PROJECT_ROOT_PATH}/_gen_deploygen.sh ${STAGE}
+
+# deploy
 cd ${MY_TMP_DIR_PATH}
 cp ${PROJECT_ROOT_PATH}/aws/serverless.yml ${MY_TMP_DIR_PATH}/
 ${SERVERLESS} --stage ${STAGE} create_domain
