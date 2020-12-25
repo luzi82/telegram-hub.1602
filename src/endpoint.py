910d9c9d072cf7a877c9cd8f4017f819db1df4fa
luzi82 <luzi82@gmail.com>
2020-12-25 06:21:28 +0000

    trace flask.request.host

--- src/endpoint.py
+++ src/endpoint.py
@@ -5,7 +5,9 @@ app = flask.Flask(__name__)
 @app.route('/')
 def index():
     now = datetime.datetime.now()
+    flask_request_host = flask.request.host
     return flask.render_template(
         'index.html.tmpl',
         now = now,
+        flask_request_host = flask_request_host,
     )
