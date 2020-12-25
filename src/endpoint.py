67c9b62fff9492b95813c727075550a6d50ee910
luzi82 <luzi82@gmail.com>
2020-12-25 06:23:01 +0000

    change from trace host to host_url

--- src/endpoint.py
+++ src/endpoint.py
@@ -5,9 +5,9 @@ app = flask.Flask(__name__)
 @app.route('/')
 def index():
     now = datetime.datetime.now()
-    flask_request_host = flask.request.host
+    flask_request_host_url = flask.request.host_url
     return flask.render_template(
         'index.html.tmpl',
         now = now,
-        flask_request_host = flask_request_host,
+        flask_request_host_url = flask_request_host_url,
     )
