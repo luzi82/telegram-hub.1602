4f62248041ab6ab2a527fc6ae5a7c9e3424f3c13
luzi82 <luzi82@gmail.com>
2020-12-25 07:18:52 +0000

    add debug, fix host and scheme in local run

--- s01_dev_run_local.sh
+++ s01_dev_run_local.sh
@@ -5,5 +5,6 @@
 . ${PROJECT_ROOT_PATH}/dev_env/venv/bin/activate
 
 cd ${PROJECT_ROOT_PATH}/src
+export FLASK_DEBUG=1
 export FLASK_APP=${PROJECT_ROOT_PATH}/src/endpoint.py
-${PROJECT_ROOT_PATH}/dev_env/venv/bin/flask run
+${PROJECT_ROOT_PATH}/dev_env/venv/bin/flask run --host 0.0.0.0
--- src/endpoint.py
+++ src/endpoint.py
@@ -1,9 +1,13 @@
 import datetime
 import flask
+import werkzeug.middleware.proxy_fix
+
 app = flask.Flask(__name__)
+app.wsgi_app = werkzeug.middleware.proxy_fix.ProxyFix(app.wsgi_app)
 
 @app.route('/')
 def index():
+    print(flask.request.headers)
     now = datetime.datetime.now()
     flask_request_host_url = flask.request.host_url
     return flask.render_template(
