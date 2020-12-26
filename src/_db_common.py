36e2922cd4d4c449caea6b10f48841c887efed47
luzi82 <luzi82@gmail.com>
2020-12-26 06:53:36 +0000

    keep table object for future use

--- src/_db_common.py
+++ src/_db_common.py
@@ -1,11 +1,15 @@
 import boto3
 import env
 
+_get_table_ret = None
 def get_table():
+  global _get_table_ret
+  if _get_table_ret: return _get_table_ret
   dynamodb = boto3.resource(
     'dynamodb',
     endpoint_url=env.DYNAMODB_ENDPOINT_URL,
     region_name=env.DYNAMODB_REGION
   )
   table = dynamodb.Table(env.DB_TABLE_NAME)
-  return table
+  _get_table_ret = table
+  return _get_table_ret
